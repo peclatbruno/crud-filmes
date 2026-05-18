26e673db8767f1436bcd022c1486e13b061f3376
Kenjihidehira <kenjihidehira999@gmail.com>
2026-05-18 20:12:03 -0300

    Guerra infinita

--- filmes.php
+++ filmes.php
@@ -2,8 +2,9 @@
 // Lista inicial de filmes
 $filmes = [
     ["id" => 1, "titulo" => "Matrix", "ano" => 1999],
-    ["id" => 2, "titulo" => "Interestelar", "ano" => 2014],
-];
+   ["id" => 2, "titulo" => "Vingadores: Guerra Infinita", "ano" => 2018],
+     ["id" => 3, "titulo" => "Vingadores: Ultimato", "ano" => 2020]
+    ];
 
 echo "<h1>Lista de Filmes</h1>";
 echo "<ul>";
