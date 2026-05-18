397b8083d8e5a366e9b5f5dbe1738a132cbea934
Noir098 <nnikolaslodi@gmail.com>
2026-05-18 19:43:28 -0300

    feat: modifica Interestelar e adiciona Capuchinho Vermelho: A Nova Aventura

--- filmes.php
+++ filmes.php
@@ -2,7 +2,8 @@
 // Lista inicial de filmes
 $filmes = [
     ["id" => 1, "titulo" => "Matrix", "ano" => 1999],
-    ["id" => 2, "titulo" => "Interestelar", "ano" => 2014],
+    ["id" => 2, "titulo" => "Interstellar", "ano" => 2014],
+    ["id" => 3, "titulo" => "Capuchinho Vermelho: A Nova Aventura", "ano" => 2011]
 ];
 
 echo "<h1>Lista de Filmes</h1>";
